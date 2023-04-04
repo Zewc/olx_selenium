3d4c1ec8782ee88de2e20642525db4761aafd7d6
Zhenia <bratchuk.zhenya@gmail.com>
2023-04-04 23:17:59 +0300

    new upgrade

--- main.py
+++ main.py
@@ -1,24 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
-from fake_useragent import UserAgent
-import time
-import random
-
-useragent = UserAgent()
+import json
 
 option = webdriver.FirefoxOptions()
 option.set_preference('dom.webdriver.enable', False)
 option.set_preference('dom.webnotifications.enable', False)
-
-# option.set_preference("general.useragent.override", useragent.random)
 
 driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=option)
 
@@ -27,41 +19,44 @@ try:
     driver.get(
         'https://www.olx.ua/d/uk/moda-i-stil/muzhskaya-obuv/q-%D0%B1%D0%B0%D1%81%D0%BA%D0%B5%D1%82%D0%B1%D0%BE%D0%BB%D1%8C%D0%BD%D1%96-%D0%BA%D1%80%D0%BE%D1%81%D1%96%D0%B2%D0%BA%D0%B8/?page=')
     driver.execute_script("window.scrollBy(0, document.body.scrollHeight)")
+    # close cookie access window
     close_popup_window = WebDriverWait(driver, 20).until(
         EC.visibility_of_element_located((By.XPATH, "/html/body/div/div[3]/div[3]/button"))).click()
-    time.sleep(10000)
+
+    data = []
+
     next_page = True
+
     while next_page == True:
-        try:
-            WebDriverWait(driver, 20).until(
-                EC.visibility_of_element_located((By.CSS_SELECTOR, ".css-oukcj3")))
-
-        except:
-            WebDriverWait(driver, 20).until(
-                EC.visibility_of_element_located((By.CSS_SELECTOR, "div.css-oukcj3:nth-child(4)")))
-
+        WebDriverWait(driver, 40).until(
+            EC.visibility_of_element_located((By.XPATH, '//div[@data-cy="l-card"]')))
         driver.execute_script("window.scrollBy(0, document.body.scrollHeight)")
-        block = driver.find_element(By.CSS_SELECTOR, ".css-oukcj3")
-        elements = block.find_elements(By.CLASS_NAME, "css-1sw7q4x")
+        elements = driver.find_elements(By.XPATH, '//div[@data-cy="l-card"]')
         for element in elements:
-            print(element.text)
-            print(element.find_element(By.TAG_NAME, 'a').get_attribute('href'))
+
+            data.append(
+                {
+                    'name': element.find_element(By.TAG_NAME, 'h6').text,
+                    'price': element.find_element(By.XPATH, '//p[@data-testid="ad-price"]').text,
+                    'location-data': element.find_element(By.XPATH, '//p[@data-testid="location-date"]').text,
+                    'link': element.find_element(By.TAG_NAME, 'a').get_attribute('href')
+                }
+            )
+
+        with open('data.json', 'w') as file:
+            json.dump(data, file, indent=4, ensure_ascii=False)
+
         try:
-            next_page_btn = WebDriverWait(driver, 20).until(EC.visibility_of_element_located(
-                (By.CSS_SELECTOR, ".pagination-list > a:nth-child(7)"))).click()
-            print('button pressed')
+            # press the button to next page
+            next_page_btn = WebDriverWait(driver, 40).until(EC.visibility_of_element_located(
+                (By.XPATH, '//a[@data-testid="pagination-forward"]'))).click()
         except Exception as ex:
             next_page = False
 
+
 except Exception as ex:
-    print('- [Exception]: ')
     print(ex)
-    time.sleep(1000)
+
 finally:
     driver.close()
     driver.quit()
-
-
-page_higher_10 = 'CSS SELECTOR - div.css-oukcj3:nth-child(4)'
-
-
--- test.py
+++ test.py
@@ -1,14 +1,14 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
-import time
+import json
 
 option = webdriver.FirefoxOptions()
 option.set_preference('dom.webdriver.enable', False)
@@ -19,34 +19,34 @@ driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()
 try:
     driver.maximize_window()
     driver.get(
-        'https://www.olx.ua/d/uk/moda-i-stil/muzhskaya-obuv/q-%D0%B1%D0%B0%D1%81%D0%BA%D0%B5%D1%82%D0%B1%D0%BE%D0%BB%D1%8C%D0%BD%D1%96-%D0%BA%D1%80%D0%BE%D1%81%D1%96%D0%B2%D0%BA%D0%B8/?page=2')
+        'https://www.olx.ua/d/uk/moda-i-stil/muzhskaya-obuv/q-%D0%B1%D0%B0%D1%81%D0%BA%D0%B5%D1%82%D0%B1%D0%BE%D0%BB%D1%8C%D0%BD%D1%96-%D0%BA%D1%80%D0%BE%D1%81%D1%96%D0%B2%D0%BA%D0%B8/?page=')
+    driver.execute_script("window.scrollBy(0, document.body.scrollHeight)")
+    # close cookie access window
+    close_popup_window = WebDriverWait(driver, 20).until(
+        EC.visibility_of_element_located((By.XPATH, "/html/body/div/div[3]/div[3]/button"))).click()
 
-#     driver.execute_script("window.scrollBy(0, document.body.scrollHeight)")
-#     close_popup_window = WebDriverWait(driver, 20).until(
-#         EC.visibility_of_element_located((By.XPATH, "/html/body/div[1]/div[10]/button"))).click()
-#
-#     html = driver.page_source
-#     with open('page.html', 'w', encoding='utf-16') as file:
-#         file.write(html)
-#
-# except Exception as ex:
-#     print(ex)
-#
-# finally:
-#     driver.close()
-#     driver.quit()
-#
-#
-#
-# xpath1 = '/html/body/div/div[1]/div[2]/form/div[5]/div/section[1]/div/ul/a'
-# xpath4 = '/html/body/div/div[1]/div[2]/form/div[5]/div/section[1]/div/ul/a[2]'
-# xpath5 = '/html/body/div/div[1]/div[2]/form/div[5]/div/section[1]/div/ul/a[2]'
-# xpath6 = '/html/body/div/div[1]/div[2]/form/div[5]/div/section[1]/div/ul/a[2]'
-# xpath10 = '/html/body/div/div[1]/div[2]/form/div[5]/div/section[1]/div/ul/a[2]'
-# xpath7 = '/html/body/div/div[1]/div[2]/form/div[5]/div/section[1]/div/ul/a[2]'
+    data = []
 
-except:
-    pass
+    WebDriverWait(driver, 40).until(
+        EC.visibility_of_element_located((By.XPATH, '//div[@data-cy="l-card"]')))
+    driver.execute_script("window.scrollBy(0, document.body.scrollHeight)")
+    elements = driver.find_elements(By.XPATH, '//div[@data-cy="l-card"]')
+    for element in elements:
+        data.append(
+            {
+                'grade': element.find_element(By.XPATH, '//span[@title]').text
+            }
+        )
+
+    with open('test.json', 'w') as file:
+        json.dump(data, file, indent=4, ensure_ascii=False)
 
 
-driver.find_element(By.XPATH, '//a[@data-testid="pagination-forward"]')
+
+
+except Exception as ex:
+    print(ex)
+
+finally:
+    driver.close()
+    driver.quit()
